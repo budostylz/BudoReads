005f5484995bbd2557ca4179402b0d682aac82d1
budostylz <lewis.shaun1@outlook.com>
2020-06-06 20:26:30 -0400

    'commit'

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+/**
+  * TODO: Nav Component
+*/
+import React from 'react'
+
+
+const Nav = (props) => {
+
+    console.log('props', props)
+
+    return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Search Books</a>
+    </nav>)
+
+}
+
+
+
+export default Nav
---
+++ src/Search.js
@@ -0,0 +1,17 @@
+/**
+  * TODO: Search Component
+*/
+
+
+import React from 'react'
+
+const Search = (props) => {
+
+    return (<div className="open-search">
+
+    </div>)
+
+}
+
+
+export default Search
